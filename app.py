72e9bcf7118bac863381db4b44ca8bd579ce6ae9
quantum-craft <y_ee182@hotmail.com>
2019-09-17 12:59:42 +0800

    Added --ip, --port as arguments

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.meshes_deep3d_face import MeshesDeep3DFaceGetMesh
+import argparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,5 +10,15 @@ api.add_resource(MeshesDeep3DFaceGetMesh,
                  '/meshes-deep3d-face/reconstruct-mesh')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    parser = argparse.ArgumentParser(
+        description='Deep3DFace Server')
 
+    parser.add_argument('--ip', default='0.0.0.0', type=str,
+                        help='ip address of the service')
+
+    parser.add_argument('--port', default='5000', type=str,
+                        help='port of the service')
+
+    args = parser.parse_args()
+
+    app.run(host=args.ip, port=args.port)
